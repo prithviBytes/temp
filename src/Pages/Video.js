a6dd8df11ed18bf8fa263727571c5e25c965c0a2
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2021-05-23 21:16:38 +0530

    feat: display video page

--- src/Pages/Video.js
+++ src/Pages/Video.js
@@ -2,9 +2,12 @@ import "../Styles/Video.css";
 import { useParams } from "react-router-dom";
 import moment from "moment";
 import faker from "faker";
+import { VideoContext } from "../Context/VideoContext";
+import { useContext } from "react";
 export default function Video() {
+  const { contextVideos } = useContext(VideoContext);
   const { id } = useParams();
-  // const currentVideo = videos.find((video) => video.embedId === id);
+  const currentVideo = contextVideos.find((video) => video.id === id);
   return (
     <div className="VideoPage">
       <div className="VideoPage-container">
@@ -13,18 +16,18 @@ export default function Video() {
             <iframe
               width="560"
               height="315"
-              src={`https://www.youtube.com/embed/${currentVideo.id.videoId}`}
+              src={`https://www.youtube.com/embed/${currentVideo.id}`}
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             ></iframe>
           </div>
-          <h2 className="VideoPage-title">{currentVideo.snippet.title}</h2>
+          <h2 className="VideoPage-title">{currentVideo.title}</h2>
           <div className="VideoPage-info">
             <div className="VideoPage-stats">
-              <span>1.1M views</span>&nbsp;&#xb7;&nbsp;
-              <span>{moment(currentVideo.snippet.publishedAt).fromNow()}</span>
+              <span>{currentVideo.views}</span>&nbsp;&#xb7;&nbsp;
+              <span>{moment(currentVideo.publishedAt).fromNow()}</span>
             </div>
             <div className="VideoPage-actions">
               <span className="VideoPage-action-button">
@@ -47,36 +50,27 @@ export default function Video() {
               src={faker.image.avatar()}
               alt=""
             />
-            <p className="VideoPage-owner">
-              {currentVideo.snippet.channelTitle}
-            </p>
+            <p className="VideoPage-owner">{currentVideo.channelTitle}</p>
           </div>
         </div>
         <div className="VideoPage-container-right">
-          {videos
-            .filter((video) => video.embedId !== currentVideo.embedId)
+          {contextVideos
+            .filter((video) => video.id !== currentVideo.id)
             .map((video) => (
               <div className="Thumbnail-horizontal">
-                <img src={video.thumbnailImage} alt="" />
+                <img src={video.images.high.url} alt="" />
                 <div className="Thumbnail-horizontal-content">
                   <h3 className="Thumbnail-horizontal-title">{video.title}</h3>
-                  <p className="Thumbnail-horizontal-owner">{video.owner}</p>
+                  <p className="Thumbnail-horizontal-owner">
+                    {video.channelTitle}
+                  </p>
                   <p className="Thumbnail-horizontal-stats">
-                    {video.views}&nbsp;&#xb7;&nbsp;{video.age}
+                    {video.views}&nbsp;&#xb7;&nbsp;
+                    {moment(video.publishedAt).fromNow()}
                   </p>
                 </div>
               </div>
             ))}
-          <div className="Thumbnail-horizontal">
-            <img src={videos[0].thumbnailImage} alt="" />
-            <div className="Thumbnail-horizontal-content">
-              <h3 className="Thumbnail-horizontal-title">{videos[0].title}</h3>
-              <p className="Thumbnail-horizontal-owner">{videos[0].owner}</p>
-              <p className="Thumbnail-horizontal-stats">
-                {videos[0].views}&nbsp;&#xb7;&nbsp;{videos[0].age}
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
